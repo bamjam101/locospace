830af05a1dc373d3e2256bee95817cbe8ae4351e
bamjam101 <herejamil123@gmail.com>
2024-08-08 19:15:24 +0530

    Integrated the admin portal and admin related interfaces, along with setting up car scene using three and react libraries

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { IconSearch } from '@tabler/icons-react'
 import Link from 'next/link'
+import { CarScene } from '@locospace/3d/src/scenes/CarScene'
 
 export default function Page() {
   return (
-    <main className="h-[calc(100vh-4rem)] ">
+    <main className="h-[calc(100vh-4rem)]">
       <div className="absolute top-16 bottom-0 left-0 right-0">
-        {/* TODO: 3D graphic comes here */}
+        <CarScene />
       </div>
       <div className="flex flex-col items-start space-y-2 font-black text-8xl">
         <div className="z-10 inline-block px-3 bg-primary mt-2">Need</div>{' '}
